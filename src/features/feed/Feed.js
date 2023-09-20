3febf2de5d690e44172476bce38d3534df729663
Zac <zac_ju@163.com>
2023-09-20 11:16:49 +0800

    Update Feed.js
    1.Add paging logic
    2.Convert to functional Component

--- src/features/feed/Feed.js
+++ src/features/feed/Feed.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Card from "../../components/Card/Card";
+import { Link, useSearchParams } from "react-router-dom";
 
 const FeedWrapper = styled.div`
   display: flex;
@@ -13,54 +14,97 @@ const Alert = styled.div`
   text-align: center;
 `;
 
+const CardLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+const PaginationBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PaginationLink = styled(Link)`
+  padding: 1%;
+  background: lightBlue;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+`;
+
 const ROOT_API = "https://api.stackexchange.com/2.2/";
 
-class Feed extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      loading: true,
-      error: "",
-    };
-  }
+// https://github.com/PacktPublishing/React-Projects/tree/ch4
 
-  async componentDidMount() {
-    try {
-      const data = await fetch(
-        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`
-      );
-      const dataJSON = await data.json();
+export default function Feed() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-      if (dataJSON) {
-        this.setState({
-          data: dataJSON,
-          loading: false,
-        });
-      }
-    } catch (error) {
-      this.setState({
-        loading: false,
-        error: error.message,
+  let params = useSearchParams();
+  let pg = params.page ? parseInt(params.page) : 1;
+  const [page, setPage] = useState(pg);
+
+  useEffect(() => {
+    fetch(
+      `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${
+        page ? `&page=${page}` : ""
+      }`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (json) {
+          setData(json);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error.message);
       });
-    }
+  }, [page]);
+
+  if (loading || error) {
+    return <Alert>{loading ? "Loading..." : error}</Alert>;
   }
 
-  render() {
-    const { data, loading, error } = this.state;
-
-    if (loading || error) {
-      return <Alert>{loading ? "Loading..." : error}</Alert>;
-    }
-
-    return (
+  return (
+    <>
       <FeedWrapper>
         {data.items.map((item) => (
-          <Card key={item.question_id} data={item} />
+          <CardLink
+            key={item.question_id}
+            to={`/questions/${item.question_id}`}
+          >
+            <Card data={item} />
+          </CardLink>
         ))}
+        <PaginationBar>
+          {page > 1 && (
+            <PaginationLink to={`/feed?page=${page - 1}`}>
+              <span
+                onClick={() => {
+                  setPage(page - 1);
+                }}
+              >
+                Previous
+              </span>
+            </PaginationLink>
+          )}
+          {data.has_more && (
+            <PaginationLink to={`/feed?page=${page + 1}`}>
+              <span
+                onClick={() => {
+                  setPage(page + 1);
+                }}
+              >
+                Next
+              </span>
+            </PaginationLink>
+          )}
+        </PaginationBar>
       </FeedWrapper>
-    );
-  }
+    </>
+  );
 }
-
-export default Feed;
